538ed86989fdeed2aba3a975b03f52a75528f844
Trust by Frazier <trustbyfrazier@gmail.com>
2026-03-25 12:06:03 +0100

    Update useAuth.tsx
    to redirect to site after clicking link

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -63,7 +63,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
   const resetPassword = async (email: string) => {
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: `${window.location.origin}/reset-password`,
+      redirectTo: `https://file-genie.netlify.app/reset-password`,
     });
     return { error };
   };
